345416fd0a754babc9c36fab8adb342025c81837
Siju Joseph <siju-joseph@netspective.in>
2025-12-03 12:56:20 +0530

    fix: change ts to runbook

--- spry_runbook.ts
+++ spry_runbook.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run -A --node-modules-dir=auto
 // Use `deno run -A --watch` in the shebang if you're contributing / developing Spry itself.
 
-import { CLI } from "https://raw.githubusercontent.com/programmablemd/spry/refs/tags/0.80.0/lib/axiom/text-ui/cli.ts";
+import { CLI } from "https://raw.githubusercontent.com/programmablemd/spry/refs/tags/0.80.0/lib/axiom/text-ui/runbook.ts";
 
-await new CLI({ defaultFiles: ["Qualityfolio.md"] }).run();
+new CLI().run();
